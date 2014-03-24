23b7627b23ea39d73e2a39e5d2348281b22eb6ba
faiez.elleuch <felleuch@gmail.com>
2014-03-24 11:31:25 +0100

    first commit

---
+++ src/integration-test/java/ITDummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+
+public class ITDummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
